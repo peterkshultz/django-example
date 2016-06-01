d5df35440c087e5b62dae361787e4102e377982a
Peter Shultz <pshultz@umich.edu>
2016-05-31 22:02:10 -0700

    Initial commit

---
+++ parent/parent/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
